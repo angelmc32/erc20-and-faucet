bc85aaa2f8b9052a4f3431cd570ce2c4dbd51276
Mel <mel@Mels-MacBook-Air.local>
2023-01-09 18:31:27 -0600

    :sparkles: Create smart contract for ERC-20 token implementation, with mintable and pausable functionality from OpenZeppeling contracts

---
+++ src/ERC20Token.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.15;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20Capped} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {ERC20PresetMinterPauser} from "../lib/openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
+contract ERC20Token is ERC20Capped, ERC20PresetMinterPauser {
+    address payable public owner;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address owner_,
+        uint256 maxSupply_,
+        uint256 initialSupply_
+    )
+        ERC20PresetMinterPauser(name_, symbol_)
+        ERC20Capped(maxSupply_ * (10**(decimals())))
+    {
+        owner = payable(owner_);
+        // Mint "initialSupply" tokens to "owner" address
+        // Similar to how
+        // 1 dollar = 100 cents
+        // 1 token = 1 * (10 ** decimals) -> 18 decimals by default
+        _mint(owner, initialSupply_ * 10**uint256(decimals()));
+    }
+
+    function mint(address to, uint256 amount)
+        public
+        virtual
+        override(ERC20PresetMinterPauser)
+    {
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "ERC20PresetMinterPauser: must have minter role to mint"
+        );
+        _mint(to, amount);
+    }
+
+    function _mint(address account, uint256 amount)
+        internal
+        virtual
+        override(ERC20Capped, ERC20)
+    {
+        require(
+            ERC20.totalSupply() + amount <= cap(),
+            "ERC20Capped: cap exceeded"
+        );
+        super._mint(account, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20, ERC20PresetMinterPauser) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
---
+++ test/ERC20Token.t.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/ERC20Token.sol";
+
+contract ERC20TokenTest is Test {
+    ERC20Token myERC20Token;
+    address testMaster = address(0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84);
+    address contractOwner = address(0x1);
+    address user0x2 = address(0x2);
+    address user0x3 = address(0x3);
+    string mockTokenName = "MockToken";
+    string mockTokenSymbol = "MOCK";
+    uint256 maxSupply = 100;
+    uint256 initialSupply = 50;
+    uint256 testAmount = 25 * (10**18);
+
+    function setUp() public {
+        myERC20Token = new ERC20Token(
+            mockTokenName,
+            mockTokenSymbol,
+            testMaster,
+            maxSupply,
+            initialSupply
+        );
+    }
+
+    function testGetTokenName() public {
+        string memory tokenName = myERC20Token.name();
+        assertEq(tokenName, mockTokenName);
+    }
+
+    function testGetTokenSymbol() public {
+        string memory tokenSymbol = myERC20Token.symbol();
+        assertEq(tokenSymbol, mockTokenSymbol);
+    }
+
+    function testGetDecimalsVariable() public {
+        assertEq(myERC20Token.decimals(), 18);
+    }
+
+    function testGetCurrentSupply() public {
+        assertEq(
+            myERC20Token.totalSupply(),
+            initialSupply * (10**myERC20Token.decimals())
+        );
+    }
+
+    function testGetMaxSupply() public {
+        assertEq(myERC20Token.cap(), maxSupply * (10**myERC20Token.decimals()));
+    }
+
+    function testGetBalanceOf() public {
+        assertEq(
+            myERC20Token.balanceOf(testMaster),
+            initialSupply * (10**myERC20Token.decimals())
+        );
+    }
+
+    function testTransfer() public {
+        myERC20Token.transfer(user0x2, testAmount);
+        assertEq(myERC20Token.balanceOf(user0x2), testAmount);
+    }
+
+    function testMint() public {
+        myERC20Token.mint(user0x3, testAmount);
+        assertEq(myERC20Token.balanceOf(user0x3), testAmount);
+    }
+
+    function testOnlyAccessControlRoleCanMint() public {
+        vm.startPrank(user0x3);
+        vm.expectRevert(
+            "ERC20PresetMinterPauser: must have minter role to mint"
+        );
+        myERC20Token.mint(user0x3, testAmount);
+        vm.stopPrank();
+    }
+
+    function testTransferFromFailsNoAllowance() public {
+        myERC20Token.mint(user0x2, testAmount);
+        vm.expectRevert("ERC20: insufficient allowance");
+        myERC20Token.transferFrom(user0x2, user0x3, testAmount);
+    }
+
+    function testApprove() public {
+        uint256 approvedAllowance = 15 * (10**18);
+        myERC20Token.mint(user0x2, testAmount);
+        vm.startPrank(user0x2);
+        myERC20Token.approve(testMaster, approvedAllowance);
+        vm.stopPrank();
+        myERC20Token.transferFrom(user0x2, user0x3, approvedAllowance);
+        assertEq(myERC20Token.balanceOf(user0x3), approvedAllowance);
+    }
+
+    function testGetAllowance() public {
+        uint256 approvedAllowance = 15 * (10**18);
+        myERC20Token.mint(user0x2, testAmount);
+        vm.startPrank(user0x2);
+        myERC20Token.approve(testMaster, approvedAllowance);
+        vm.stopPrank();
+        assertEq(
+            myERC20Token.allowance(user0x2, testMaster),
+            approvedAllowance
+        );
+    }
+
+    function testIncreaseAllowance() public {
+        uint256 approvedAllowance = 15 * (10**18);
+        myERC20Token.mint(user0x2, testAmount);
+        vm.startPrank(user0x2);
+        myERC20Token.approve(testMaster, approvedAllowance);
+        myERC20Token.increaseAllowance(testMaster, 10 * (10**18));
+        vm.stopPrank();
+        assertEq(myERC20Token.allowance(user0x2, testMaster), testAmount);
+    }
+
+    function testDecreaseAllowance() public {
+        uint256 approvedAllowance = 15 * (10**18);
+        myERC20Token.mint(user0x2, testAmount);
+        vm.startPrank(user0x2);
+        myERC20Token.approve(testMaster, approvedAllowance);
+        myERC20Token.decreaseAllowance(testMaster, 10 * (10**18));
+        vm.stopPrank();
+        assertEq(myERC20Token.allowance(user0x2, testMaster), 5 * (10**18));
+    }
+
+    function testTransferFrom() public {
+        uint256 approvedAllowance = 15 * (10**18);
+        myERC20Token.mint(user0x2, testAmount);
+        vm.startPrank(user0x2);
+        myERC20Token.approve(testMaster, approvedAllowance);
+        myERC20Token.increaseAllowance(testMaster, 10 * (10**18));
+        vm.stopPrank();
+        myERC20Token.transferFrom(user0x2, user0x3, testAmount);
+        assertEq(myERC20Token.balanceOf(user0x3), testAmount);
+    }
+}
